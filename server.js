91803dfaf609f0ea01869a3b9b8520a49e2dfdb0
yawningstudent <31552267+yawningstudent@users.noreply.github.com>
2019-09-23 22:40:54 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,9 +2,8 @@ const http = require("http");
 let server = new http.Server();
 server.listen(3000, "127.0.0.1");
 server.on("request", (req, res)=>{
-    console.log("Hello world!\n");
     res.writeHead(200);
-    res.end();
+    res.end("Hello World");
 });
 
 module.exports.server = server
